19c87e4d46a0e35f8147a76f21308e7b9a950a8d
Nithin Thampi <nthampi@walmart.com>
2019-10-27 10:39:41 +0530

    Updates

---
+++ precache-manifest.5169e5709ab982519cca92de45103ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e89896c2bc169ee59091b4b3e0b962e",
+    "url": "/react-app-deploy/index.html"
+  },
+  {
+    "revision": "6a7d06cd04cf8e0027da",
+    "url": "/react-app-deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f78a07612e49a458bd38",
+    "url": "/react-app-deploy/static/js/2.abd9833f.chunk.js"
+  },
+  {
+    "revision": "6a7d06cd04cf8e0027da",
+    "url": "/react-app-deploy/static/js/main.62e44a0c.chunk.js"
+  },
+  {
+    "revision": "db837213c44ab6ad0a85",
+    "url": "/react-app-deploy/static/js/runtime-main.abe79d2d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app-deploy/static/media/logo.25bf045c.svg"
+  }
+]);
